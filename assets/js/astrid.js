a7248c203352dec27b55585a15861ac01709b69e
Paco (PC despacho) <fjmartin@dcom.upv.es>
2021-12-10 12:19:18 +0100

    updating team

--- assets/js/astrid.js
+++ assets/js/astrid.js
@@ -1,23 +1,28 @@
 
 
-
 let contactos = document.querySelectorAll("ul.icons > li > a");
-contactos.forEach( function(item) {
-    item.addEventListener("mouseover", function(){
-        let p = document.getElementById("contacto");
+contactos.forEach( function(item) {    
+    item.addEventListener("mouseover", function(){       
+        let url = "";
         switch (item.dataset["contacto"]){
-            case "mail": p.innerHTML = "agonzal@dcom.upv.es"; break;
-            case "github": p.innerHTML = "https://github.com/GTAC-ITEAM-UPV/Astrid"; break;
-            case "web": p.innerHTML = "http://www.iteam.upv.es/group/gtac/";  break;
-            case "twitter": p.innerHTML = "http://www.iteam.upv.es/group/gtac/"; break;
-            case "facebook": p.innerHTML = "http://www.iteam.upv.es/group/gtac/"; break;
-            case "instagram": p.innerHTML = "http://www.iteam.upv.es/group/gtac/"; break;
+            case "mail":      url = "agonzal@dcom.upv.es"; break;
+            case "github":    url = "https://github.com/GTAC-ITEAM-UPV/Astrid"; break;
+            case "web":       url = "https://gtac.webs.upv.es/";  break;
+            case "twitter":   url = "https://gtac.webs.upv.es/"; break;
+            case "facebook":  url = "https://gtac.webs.upv.es/"; break;
+            case "instagram": url = "https://gtac.webs.upv.es/"; break;
         }
+
+        navigator.clipboard.writeText(url);        
+        console.log("clipboard: " + url);
+        document.getElementById("contacto").innerHTML = "<b>" + url + "</b> copied to the clipboard";
     });
     item.addEventListener("mouseleave", function() {
         document.getElementById("contacto").innerHTML = "&nbsp;"
     })
+    
 })
 
 
 document.getElementById("anyo").innerHTML = new Date().getFullYear();
+
